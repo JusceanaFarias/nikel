31aecf66ddc1584e84b5b078589210256c375a79
Jusceana <jju.farias@gmail.com>
2023-08-28 15:08:43 -0300

    Iniciando o Git

---
+++ public/js/index.js
@@ -0,0 +1,102 @@
+const mymodal = new bootstrap.Modal("#register-modal");
+let logged = sessionStorage.getItem("logged");
+const session = localStorage.getItem("session");
+
+checkLogged();
+
+document.getElementById("login-form").addEventListener("submit", function(e) {
+ e.preventDefault();
+ 
+ const email = document.getElementById("email-input").value;
+ const password = document.getElementById("password-input").value;
+ const checksession = document.getElementById("session-check").checked;
+    
+ const account = getAccount(email);
+
+ if(!account) {
+    alert("opps! Verifique o usuário ou a senha.")
+    return;
+
+if(account.password !== password) {
+    alert("opps! Verifique o usuário ou a senha.");
+    return;
+}
+
+saveSession(email, checksession);
+
+
+    window.location.href = "home.html";
+ }
+});
+
+document.getElementById("creat-form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    const email = document.getElementById("email-creat-input").value;
+    const password = document.getElementById("password-creat-input").value;
+    const pass = document.getElementById("pass-creat-input").value;
+
+    if(email.length < 5) {
+        alert("Preencha o campo com um email válido.");
+        return;
+    }
+    
+    if(password.length < 4) {
+        alert("Preencha a senha com no mínimo 4 digitos");
+        return;
+    }
+
+    if(password.length) {
+        alert("Senhas diferetes!");
+        return false;
+    }else {
+        return true;
+    }
+
+    saveAccount ({
+        login: email,
+        password: password,
+        transactions: []
+    });
+
+    mymodal.hide();
+
+   alert("Conta criada com sucesso");
+
+});
+
+function checkLogged() {
+   if(session) {
+    sessionStorage.setItem("logged", session);
+    logged = session;
+   }
+
+   if(logged) {
+    saveSession(logged, session);
+
+    window.location.href = "home.html";
+   }
+}
+
+
+function saveAccount(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
+}
+
+function saveSession(data, saveSession) {
+    if(saveSession) {
+        localStorage.setItem("session", data);
+    }
+    sessionStorage.setItem("logged", data)
+}
+
+function getAccount(key) {
+    const account = localStorage.getItem(key)
+
+    if(account) {
+        return JSON.parse(account);
+}
+
+return
+
+}
